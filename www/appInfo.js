08a94b8dfac2003bb7393f20a6032a8f7ab8de15
Ayyaz Zafar <ayyaz.zaffar@gmail.com>
2018-09-30 21:18:54 +0500

    test

---
+++ www/appInfo.js
@@ -0,0 +1,20 @@
+﻿/**
+ * Cordova AppInfo Plugin
+ */
+
+var AppInfo = (function () {
+
+    function AppInfo() {}
+
+    AppInfo.prototype.echo = function (echoValue, successCallback) {
+        cordova.exec(successCallback, function () {
+            console.log('Error')
+        }, 'AppInfo', 'echo', [echoValue]);
+    };
+
+    return AppInfo;
+
+})();
+
+var appInfo = new AppInfo();
+module.exports = appInfo;
